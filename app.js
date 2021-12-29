86bab5029426080ce5743fd6f533a28f9bd27600
Jorge <jorge.caprarella@gmail.com>
2021-12-29 10:51:49 -0300

    cambio parametro a networkidle0

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
 
             const page = await browser.newPage();
             await page.goto(urlToScreenshot, {
-			  waitUntil: 'networkidle2',
+			  waitUntil: 'networkidle0',
 			});
 			await page.screenshot().then(function(buffer) {
 				res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
